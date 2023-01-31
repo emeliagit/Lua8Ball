79cbcb7ff0cc0825efba66d9dc8960e6d0f08572
Emeliaisdumb <66752943+emeliagit@users.noreply.github.com>
2023-01-31 11:40:02 -0500

    Update and rename main.lua to lua8ball.lua

---
+++ lua8ball.lua
@@ -0,0 +1,53 @@
+print("Hello! Plese insert your name.")
+name = io.read()
+print("Hello, " .. name .. "! I'm an 8 ball, and it seems you have a question. Please, do go ahead and type it, then hit the 'enter' button.")
+question = io.read()
+print(name .. " asks, ".. "'"  .. question .. "'")
+number = math.random(1, 20)
+answer = ""
+
+-- Affirmative Answers
+if number == 1 then 
+  answer = "It is certain."
+elseif number == 2 then
+  answer = "It is decidedly so."
+elseif number == 3 then
+  answer = "Without a doubt."
+elseif number == 4 then 
+  answer = "Yes, definitely."
+elseif number == 5 then 
+  answer = "You may rely on it."
+elseif number == 6 then 
+  answer = "As I see it, yes."
+elseif number == 7 then 
+  answer = "Most likely."
+elseif number == 8 then 
+  answer = "Outlook good."
+elseif number == 9 then 
+  answer = "Yes."
+elseif number == 10 then 
+  answer = "Signs point to 'yes.'"
+-- Non-committal answers
+elseif number == 11 then 
+  answer = "Reply hazy, try again."
+elseif number == 12 then 
+  answer = "Ask again later."
+elseif number == 13 then 
+  answer = "Better not tell you now."
+elseif number == 15 then 
+  answer = "Cannot predict now."
+elseif number == 16 then 
+  answer = "Concentrate and ask again."
+-- Negative Answers
+elseif number == 17 then 
+  answer = "Don't count on it."
+elseif number == 18 then 
+  answer = "My reply is 'no.'"
+elseif number == 19 then 
+  answer = "My sources say 'no.'"
+elseif number == 20 then
+  answer = "Outlook not so good."
+else
+  answer = "Very doubtful."
+end
+print(answer)
